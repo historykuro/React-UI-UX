d92104e01dff3fbb5f55f283f0e0df566698a568
historykuro <caubeprono2@gmail.com>
2022-12-13 20:29:11 +0700

    first commit

---
+++ src/components/index.jsx
@@ -0,0 +1,8 @@
+export { default as Billing } from "./Billing";
+export { default as Button } from "./Button";
+export { default as CardDeal } from "./CardDeal";
+export { default as GetStarted } from "./GetStarted";
+export { default as Hero } from "./Hero";
+export { default as Navbar } from "./Navbar";
+export { default as Stats } from "./Stats";
+export { default as Testimonials } from "./Testimonials";
